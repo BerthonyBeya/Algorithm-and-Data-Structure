914c6006c2d88cc8b0db5ec1609d98b60e8bac41
Berthony Beya <berthonybeyadropbox@gmail.com>
2024-11-28 12:40:51 +0530

    updated

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
+//* Given an array with a nested array inside of it, [1, 2, 3, [4, 5, 6], 1]
+//* Display all the elements in one array in the same order.
+//* Solution
+/* function combineArrays(array) {
+  const combineArrays = [];
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === "object") {
+      array[i].forEach((element) => {
+        combineArrays.push(element);
+      });
+    } else {
+      combineArrays.push(array[i]);
+    }
+  }
+
+  return combineArrays;
+}
+
+console.log(combineArrays([1, 2, 3, [4, 5, 6], 1])); */
+
+//* Given an array with 0s at the end [1, 2, 3, 4, 5, 0, 0], display those zeros at the beginning of the array [0, 0, 1, 2, 3, 4, 5].
+/* function zerosAtTheFront(array) {
+  const zeros = [];
+  const rest = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 0) {
+      zeros.push(array[i]);
+    } else {
+      rest.push(array[i]);
+    }
+  }
+
+  //* return [...zeros, ...rest];
+  return zeros.concat(rest);
+}
+
+console.log(zerosAtTheFront([1, 2, 3, 4, 5, 0, 0])); */
+
+//* //////////////////////////////////////////////////////////////////////////////////////
+//* //////////////////////////////////////////////////////////////////////////////////////
+//* THE ALGORITHMS AND DATA STRUCTURE COURSE BIGINS HERE!!! //////////////////////////////
+//* //////////////////////////////////////////////////////////////////////////////////////
+//* //////////////////////////////////////////////////////////////////////////////////////
 //* Given two arrays, create a function that lets a user know (true / false)
 //* wether these two arrays contain any common item
 //* For example:
@@ -35,5 +78,3 @@ console.log(commonItem(["a", "b", "c", "x"], ["z", "y", "i", "c"])); */
 }
 
 console.log(commonItem(["a", "b", "c", "x"], ["z", "y", "i", "x"])); */
-
-
