11ba226ec950ec22d1fa799a31757a9858bb2e26
Berthony Beya <berthonybeyagithub@gmail.com>
2024-12-21 20:35:47 +0530

    More algorithms added

--- index.js
+++ index.js
@@ -80,8 +80,22 @@ console.log(commonItem(["a", "b", "c", "x"], ["z", "y", "i", "c"])); */
 console.log(commonItem(["a", "b", "c", "x"], ["z", "y", "i", "x"])); */
 
 //* Solution 3
-function commonItem(array1, array2) {
- //* Put code here.  
+/* function commonItem(array1, array2) {
+  const obj = {};
+  for (let i = 0; i < array1.length; i++) {
+    if (!obj[array1[i]]) {
+      obj[array1[i]] = true;
+    }
+  }
+
+  for (let j = 0; j < array2.length; j++) {
+    if (obj[array2[j]]) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 console.log(commonItem(["a", "b", "c", "x"], ["z", "y", "i", "x"]));
+ */
